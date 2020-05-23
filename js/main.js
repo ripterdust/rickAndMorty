59c2c63790261c148429bb3e0aeed675e403b531
ripterdust <bryantello2010@hotmail.com>
2020-05-22 20:20:45 -0600

    First commit on Rick and Morty APIÂ

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Rick and morty api
+
+// const url = 'https://rickandmortyapi.com/api/';
+
+// fetch(url + 'character')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data.results);
+//     })
+//     .catch(err => console.log(err));
