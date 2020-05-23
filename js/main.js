a89af52a867b7fddea012009e21181967aeede5a
ripterdust <bryantello2010@hotmail.com>
2020-05-22 21:09:57 -0600

    Consumida la API, ya solo hace falta embellecer las targetas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 // Rick and morty api
 
-// const url = 'https://rickandmortyapi.com/api/';
+const url = 'https://rickandmortyapi.com/api/';
 
-// fetch(url + 'character')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data.results);
-//     })
-//     .catch(err => console.log(err));
+getAllCharacters = () => {
+    fetch(url + 'character')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        var container = document.querySelector('#container');
+        container.innerHTML = '';
+        // Bucle que imprime las cartas
+        for (let i = 0; i < data.results.length; i++) {
+            container.innerHTML += `
+            <div class="card">
+                <img src="${data.results[i].image}" alt="">
+                <div class="data">
+                    <p>Nombre: ${data.results[i].name}</p>
+                    <p>Especie: ${data.results[i].species}</p>
+                    <p>Género: ${data.results[i].gender}</p>
+                    <p></p>
+                </div>
+            </div>
+            `;
+            
+        }
+        
+    })
+    .catch(err => console.log(err));
+}
+
+getAllCharacters()
